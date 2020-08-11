bb3a7b1e8d02218540ccea248ea2dc4ec86f9e69
apsmith <apsmith@ccs.neu.edu>
2020-08-11 18:56:19 -0400

    started implementing authentication logic

--- auth.js
+++ auth.js
@@ -1,11 +1,15 @@
 const Router = require('express');
 const bodyParser = require('body-parser');
-const { OAuth2Client } = require('google-auth-library');
+// const { OAuth2Client } = require('google-auth-library');
 const jwt = require('jsonwebtoken');
 const { AuthenticationError } = require('apollo-server-express');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const { add } = require('./item.js');
+const { getDb } = require('./db.js');
 
 let { JWT_SECRET } = process.env;
+const collection = 'users';
 
 if (!JWT_SECRET) {
   if (process.env.NODE_ENV !== 'production') {
@@ -21,6 +25,12 @@ const routes = new Router();
 routes.use(bodyParser.json());
 const origin = process.env.UI_SERVER_ORIGIN || 'http://localhost:8000';
 routes.use(cors({ origin, credentials: true }));
+
+async function register(parent, { username, password }) {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await add(parent, { username, password: hashedPassword }, collection);
+  return user;
+}
 
 function getUser(req) {
   const token = req.cookies.jwt;
@@ -38,27 +48,55 @@ routes.post('/signin', async (req, res) => {
   if (!JWT_SECRET) {
     res.status(500).send('Missing JWT_SECRET. Refusing to authenticate.');
   }
-  const googleToken = req.body.google_token;
-  if (!googleToken) {
-    res.status(400).send({ code: 400, message: 'Missing token' });
-    return;
+  // const googleToken = req.body.google_token;
+  // if (!googleToken) {
+  //   res.status(400).send({ code: 400, message: 'Missing token' });
+  //   return;
+  // }
+
+  // const client = new OAuth2Client();
+  // let payload;
+  // try {
+  //   const ticket = await client.verifyIdToken({ idToken: googleToken });
+  //   payload = ticket.getPayload();
+  // } catch (error) {
+  //   res.status(403).send('Invalid credentials');
+  // }
+
+  // const { given_name: givenName, name, email } = payload;
+  // const credentials = {
+  //   signedIn: true, givenName, name, email,
+  // };
+
+  // const token = jwt.sign(credentials, JWT_SECRET);
+
+  const { username, password } = req.body.user;
+  const db = getDb();
+  const user = await db.collection(collection).findOne({ username });
+  if (!user) {
+    throw new AuthenticationError('Invalid Login');
   }
 
-  const client = new OAuth2Client();
-  let payload;
-  try {
-    const ticket = await client.verifyIdToken({ idToken: googleToken });
-    payload = ticket.getPayload();
-  } catch (error) {
-    res.status(403).send('Invalid credentials');
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatch) {
+    throw new AuthenticationError('Invalid Login');
   }
 
-  const { given_name: givenName, name, email } = payload;
   const credentials = {
-    signedIn: true, givenName, name, email,
+    signedIn: true,
+    id: user.id,
+    username: user.username,
   };
 
-  const token = jwt.sign(credentials, JWT_SECRET);
+  const token = jwt.sign(
+    credentials,
+    JWT_SECRET,
+    {
+      expiresIn: '30d',
+    },
+  );
+
   res.cookie('jwt', token, { httpOnly: true, domain: process.env.COOKIE_DOMAIN });
 
   res.json(credentials);
@@ -86,9 +124,15 @@ function mustBeSignedIn(resolver) {
 }
 
 function resolveUser(_, args, { user }) {
+  console.log('In auth.resolveUser, returning:');
+  console.log(user);
   return user;
 }
 
 module.exports = {
-  routes, getUser, mustBeSignedIn, resolveUser,
+  routes,
+  getUser,
+  mustBeSignedIn,
+  resolveUser,
+  register,
 };
